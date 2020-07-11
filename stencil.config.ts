c65083c339a9b41bf86b5d4c32309ab366e3997a
jfcere <jfcere@sherweb.com>
2020-07-11 12:52:47 -0400

    Rename repository

--- stencil.config.ts
+++ stencil.config.ts
@@ -2,7 +2,7 @@ import { Config } from '@stencil/core';
 import { sass } from '@stencil/sass';
 
 export const config: Config = {
-  namespace: 'stencil-sample',
+  namespace: 'stencil-components',
   taskQueue: 'async',
   outputTargets: [
     {
